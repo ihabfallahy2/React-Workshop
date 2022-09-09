f7034253e2df8b7c1ef58d313834cf4d11ded429
ihabfallahy2 <ihab2000@hotmail.es>
2022-09-10 00:14:37 +0200

    router-implementation

--- src/Services/spaceApi.js
+++ src/Services/spaceApi.js
@@ -10,6 +10,17 @@ export async function GetAllLaunches(){
     }
 }
 
+export async function GetLauncheByFlightNumber(flightNumber){
+    try {
+        const response = await fetch(API_URL + "launches/"+flightNumber);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
 export async function GetAllRockets(){
     try {
         const response = await fetch(API_URL + "rockets");
